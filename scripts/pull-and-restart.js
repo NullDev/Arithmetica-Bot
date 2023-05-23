f4c11d144812775bfeac98c885b23d0b8391ec38
NullDev (Shadow) <chris@nulldev.org>
2023-05-23 14:24:35 +0200

    made deployment script bit more verbose

--- scripts/pull-and-restart.js
+++ scripts/pull-and-restart.js
@@ -1,16 +1,23 @@
 #!/usr/bin/node
 import path from "node:path";
 import { exec } from "node:child_process";
-import { promisify } from "node:util";
 import checkDependencies from "check-dependencies";
 
 // ========================= //
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const execAsync = promisify(exec);
 const ecosystemPath = path.resolve("./pm2.ecosystem.json");
 const isNpmInstallNeeded = async() => !(await checkDependencies())?.depsWereOk;
+
+const execAsync = command => new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+        if (stdout) console.log(stdout.trim());
+        if (stderr) console.error(stderr);
+        if (error) return reject(error);
+        return resolve({ stdout, stderr });
+    });
+});
 
 try {
     console.log("[pull-and-restart] Pulling latest changes from git...");
@@ -18,20 +25,26 @@ try {
     console.log("[pull-and-restart] Done.");
 
     console.log("[pull-and-restart] Checking for missing dependencies...");
-    const npmCiNeeded = await isNpmInstallNeeded();
-    if (npmCiNeeded){
+    if (await isNpmInstallNeeded()){
         console.log("[pull-and-restart] Missing or outdated dependencies detected. Installing...");
         await execAsync("npm ci");
         console.log("[pull-and-restart] Done.");
     }
     else console.log("[pull-and-restart] No missing dependencies detected. Skipping...");
 
-    console.log("[pull-and-restart] Restarting...");
+    console.log("[pull-and-restart] Restarting via pm2...");
     await execAsync(`pm2 startOrReload ${ecosystemPath}`);
     console.log("[pull-and-restart] Done.");
+
+    console.log("[pull-and-restart] Saving pm2 list...");
+    await execAsync("pm2 save");
+    console.log("[pull-and-restart] Done.");
+
+    console.log("[pull-and-restart] Pull and restart completed successfully.");
 }
 catch (e){
     console.log("[pull-and-restart] An error occurred:");
     console.error(e);
+    console.log("[pull-and-restart] Pull and restart failed.");
     process.exit(1);
 }
