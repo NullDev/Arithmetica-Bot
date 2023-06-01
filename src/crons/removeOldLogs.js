35042933264c4524c277f90902d7634dcf03b844
NullDev (Shadow) <chris@nulldev.org>
2023-06-01 19:50:23 +0200

    fixed logging issues

--- src/crons/removeOldLogs.js
+++ src/crons/removeOldLogs.js
@@ -7,6 +7,30 @@ import Log from "../util/log.js";
 // ========================= //
 
 /**
+ * Get date from filename
+ *
+ * @param {string} filename
+ * @return {Number | null}
+ */
+const getDateFromFilename = function(filename){
+    // Format: roleshop-DD-MM-YYYY-output.log and roleshop-DD-MM-YYYY-errors.log
+    const regex = /(\d{2})-(\d{2})-(\d{4})/;
+    const match = filename.match(regex);
+
+    if (match){
+        const day = Number(match[1]);
+        const month = Number(match[2]);
+        const year = Number(match[3]);
+
+        const dateX = new Date(year, month - 1, day);
+        const date = new Date(dateX.getTime() + Math.abs(dateX.getTimezoneOffset() * 60000));
+
+        return date.getTime();
+    }
+    return null;
+};
+
+/**
  * Remove logs that are older than 7 days
  */
 const removeOldLogs = async() => {
@@ -22,18 +46,19 @@ const removeOldLogs = async() => {
 
     const allLogs = [...logs, ...eLogs];
 
-    const sevenDaysAgo = new Date();
-    sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+    const sevenDaysAgoX = new Date();
+    sevenDaysAgoX.setDate(sevenDaysAgoX.getDate() - 7);
 
-    allLogs.forEach(async f => {
-        // Format: roleshop-DD-MM-YYYY-output.log and roleshop-DD-MM-YYYY-errors.log
-        const fileDateFromName = new Date(f.split("-").slice(1, 4).join("-").split(".")[0]);
-        if (fileDateFromName < sevenDaysAgo){
+    const sevenDaysAgo = (new Date(sevenDaysAgoX.getTime() + Math.abs(sevenDaysAgoX.getTimezoneOffset() * 60000))).getTime();
+
+    await Promise.all(allLogs.map(async f => {
+        const fileDateFromName = getDateFromFilename(f);
+        if (!fileDateFromName || fileDateFromName < sevenDaysAgo){
             await fs.unlink(f).catch(e => Log.error(`[CRON] Could not remove old log ${f}`, e));
             Log.done(`[CRON] Removed old log ${f}.`);
             ++deletedLogs;
         }
-    });
+    }));
 
     Log.done(`[CRON] Removed ${deletedLogs} old log${deletedLogs === 1 ? "" : "s"}.`);
 };
