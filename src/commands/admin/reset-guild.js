2aca13508538f40b2f0d1e53e3880b218eedbf21
NullDev (Shadow) <chris@nulldev.org>
2023-08-27 17:06:37 +0200

    added guild reset command

---
+++ src/commands/admin/reset-guild.js
@@ -0,0 +1,53 @@
+import path from "node:path";
+import { SlashCommandBuilder, PermissionFlagsBits, ButtonStyle } from "discord.js";
+import { QuickDB } from "quick.db";
+import translations from "../../../locales/commands/translations.js";
+import createYesNoInteraction from "../../events/yesNoInteraction.js";
+import __ from "../../service/i18n.js";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+const guildDb = new QuickDB({
+    filePath: path.resolve("./data/guild_data.sqlite"),
+});
+
+const userDb = new QuickDB({
+    filePath: path.resolve("./data/user_data.sqlite"),
+});
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("reset-guild")
+        .setDescription(translations.reset_guild.desc)
+        .setDescriptionLocalizations(translations.reset_guild.translations)
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+    /**
+     * @param {import("discord.js").CommandInteraction} interaction
+     */
+    async execute(interaction){
+        createYesNoInteraction(interaction, {
+            promptText: await __("replies.reset_guild.are_you_sure")(interaction.guildId),
+            yesText: await __("replies.reset_guild.confirm")(interaction.guildId),
+            noText: await __("replies.reset_guild.abort")(interaction.guildId),
+            noStyle: ButtonStyle.Secondary,
+            yesStyle: ButtonStyle.Danger,
+            showNoFirst: true,
+        }).then(async(answer) => {
+            if (answer === "yes"){
+                await guildDb.delete(`guild-${interaction.guildId}.lastUser`);
+                await guildDb.set(`guild-${interaction.guildId}.count`, 0);
+                await guildDb.set(`guild-${interaction.guildId}.lastCountString`, "");
+                await userDb.delete(`guild-${interaction.guildId}`);
+
+                await interaction.followUp({ content: await __("replies.reset_guild.sucess")(interaction.guildId) });
+            }
+            else if (answer === "no"){
+                await interaction.followUp({ content: await __("generic.aborted")(interaction.guildId) });
+            }
+        });
+    },
+};
---
+++ src/events/yesNoInteraction.js
@@ -0,0 +1,69 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
+import __ from "../service/i18n.js";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+/* eslint-disable no-param-reassign */
+
+/**
+ * Create a yes/no interaction
+ *
+ * @param {import("discord.js").CommandInteraction | import("discord.js").StringSelectMenuInteraction} interaction
+ * @param {object} options
+ * @returns {Promise<string>}
+ */
+const createYesNoInteraction = async function(interaction, {
+    promptText = null,
+    yesText = null,
+    noText = null,
+    yesStyle = ButtonStyle.Success,
+    noStyle = ButtonStyle.Danger,
+    showNoFirst = false,
+}){
+    promptText ??= await __("generic.are_you_sure")(interaction.guildId);
+    yesText ??= await __("generic.yes")(interaction.guildId);
+    noText ??= await __("generic.no")(interaction.guildId);
+
+    const yes = new ButtonBuilder()
+        .setCustomId("yes")
+        .setLabel(yesText)
+        .setStyle(yesStyle);
+
+    const no = new ButtonBuilder()
+        .setCustomId("no")
+        .setLabel(noText)
+        .setStyle(noStyle);
+
+    const row = showNoFirst
+        ? new ActionRowBuilder().addComponents(no, yes)
+        : new ActionRowBuilder().addComponents(yes, no);
+
+    await interaction.deferReply();
+
+    const response = await interaction.followUp({
+        content: promptText,
+        // @ts-ignore
+        components: [row],
+    });
+
+    const collectorFilter = i => i.user.id === interaction.user.id;
+    try {
+        const confirmation = await response.awaitMessageComponent({
+            filter: collectorFilter,
+            time: 60000,
+        });
+        await response.edit({ components: [] });
+        return confirmation.customId;
+    }
+    catch (e){
+        await response.edit({ components: [] });
+        await interaction.followUp({
+            content: await __("errors.yn_timeout")(interaction.guildId),
+        });
+        return "timeout";
+    }
+};
+
+export default createYesNoInteraction;
