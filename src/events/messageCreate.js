1dfc4681484944936cbec44c4434ae5901fbdbc6
NullDev (Shadow) <chris@nulldev.org>
2024-01-18 19:27:47 +0100

    overhauled queue system

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,21 +1,10 @@
-import countingService from "../service/countingService.js";
+import QueueManager from "../service/queueManager.js";
 
 // ========================= //
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const QUEUE = [];
-
-/**
- * Work through the queue
- */
-const handleQueue = async function(){
-    while (QUEUE.length > 0){
-        const message = QUEUE[0];
-        await countingService(message);
-        QUEUE.shift();
-    }
-};
+const queueManager = new QueueManager();
 
 /**
  * Handle messageCreate event
@@ -24,11 +13,7 @@ const handleQueue = async function(){
  * @return {Promise<void>}
  */
 const messageCreate = async function(message){
-    if (message.author.bot) return;
-
-    QUEUE.push(message);
-
-    if (QUEUE.length === 1) await handleQueue();
+    await queueManager.enqueueMessage(message);
 };
 
 export default messageCreate;
