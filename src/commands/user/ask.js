a6c658be049f098ac6b2d1f21b3583725c8ae396
arellak <nimble0@protonmail.com>
2024-03-05 21:39:14 +0100

    added image generation to append multiple pods from the response

--- src/commands/user/ask.js
+++ src/commands/user/ask.js
@@ -1,6 +1,7 @@
-import { SlashCommandBuilder } from "discord.js";
+import { AttachmentBuilder, SlashCommandBuilder } from "discord.js";
 import translations from "../../../locales/commands/translations.js";
 import { config } from "../../../config/config.js";
+import { createCanvas, loadImage } from "canvas";
 
 // ========================= //
 // = Copyright (c) NullDev = //
@@ -29,10 +30,50 @@ const getQuery = async(query, args) => {
         if(res.queryresult === undefined
             || res.queryresult.pods.length < 2
             || res.queryresult.pods[1].subpods.length < 1){
-            return "No result";
+            return [];
         }
-        return res.queryresult.pods[1].subpods[0].img.src;
+
+        if(!!res.queryresult.pods[0].primary){
+            return res.queryresult.pods;
+        }
+
+        const pods = res.queryresult.pods.filter(pod => !!pod.primary);
+        return [res.queryresult.pods[0], ...pods];
     });
+};
+
+const generateImage = async(pods) => {
+    const width = Math.max(pods.reduce((max, pod) => Math.max(pod.subpods[0].img.width, max), 0), 250) + 20;
+    const height = pods.reduce((totalHeight, pod) => totalHeight + pod.subpods[0].img.height, 80);
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.font = "bold 15px sans-serif";
+    ctx.fillStyle = "black";
+    const lineHeight = 20;
+
+    let currentHeight = 0;
+    for(let i = 0; i < pods.length; i++){
+        const pod = pods[i];
+        console.log(pod.title);
+        const inputImagePod = pod.subpods[0].img;
+        const image = await loadImage(inputImagePod.src);
+        const inputText = pod.title;
+        if(i === 0){
+            ctx.fillText(inputText, 10, currentHeight + lineHeight);
+            ctx.drawImage(image, 30, currentHeight + lineHeight + 5);
+            currentHeight += image.height + lineHeight;
+        }
+        else{
+            ctx.fillText(inputText, 10, currentHeight + lineHeight + 8);
+            ctx.drawImage(image, 30, currentHeight + lineHeight + 13);
+            currentHeight += image.height + lineHeight + 8;
+        }
+    }
+
+    return canvas.toBuffer("image/png");
 };
 
 export default {
@@ -52,8 +93,12 @@ export default {
     async execute(interaction){
         await interaction.deferReply();
         const userInput = interaction.options.get("input");
+        const result = await getQuery(userInput?.value);
+        const buffer = await generateImage(result);
+        const resultImage = new AttachmentBuilder(buffer).setName("result.png");
+
         return await interaction.editReply({
-            content: await getQuery(userInput?.value) ?? "No result",
+            files: [resultImage],
         });
     },
 };
