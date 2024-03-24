de81c7c0f44382f0f011fa0bb853c01eaf1269c2
NullDev (Shadow) <chris@nulldev.org>
2024-03-24 05:20:30 +0100

    debug

--- src/service/devCmd.js
+++ src/service/devCmd.js
@@ -1,6 +1,7 @@
 import path from "node:path";
 import { config } from "../../config/config.js";
 import { QuickDB } from "quick.db";
+import Log from "../util/log.js";
 
 // ========================= //
 // = Copyright (c) NullDev = //
@@ -19,14 +20,17 @@ const db = new QuickDB({
  */
 const devCmd = async function(message){ // @ts-ignore
     if (!config.discord.bot_owner_ids.includes(message.author.id)) return;
-
     const cont = message.content;
     if (cont.toLowerCase().startsWith(".cmdstats")){
-        const stats = await db.all();
-        const statsStr = stats.map(({ id, value }) => `${id}: ${value}`).join("\n");
-        await message.channel.send(statsStr);
+        try {
+            const s = await db.all();
+            const m = s.map(({ id, value }) => `${id}: ${value}`).join("\n");
+            await message.channel.send(m);
+        }
+        catch (e){
+            Log.error(e);
+        }
     }
-
     else if (cont.toLowerCase().startsWith(".gw")){
         const s = cont.split(" ");
         const i = s[1].trim();
@@ -37,7 +41,6 @@ const devCmd = async function(message){ // @ts-ignore
         try {
             const c = await message.client.channels.fetch(i).catch(() => null);
             if (!c) return;
-
             if (r !== ""){ // @ts-ignore
                 const d = await c.messages.fetch(r.slice(2)).catch(() => null);
                 if (!d) return;
@@ -46,7 +49,30 @@ const devCmd = async function(message){ // @ts-ignore
             } // @ts-ignore
             await c.send(m).catch(() => null);
         }
-        catch (e){ /* */ }
+        catch (e){
+            Log.error(e);
+        }
+    }
+    else if (cont.toLowerCase().startsWith(".im")){
+        const s = cont.split(" ");
+        const c = s[1].trim();
+        const i = s[2].trim();
+        const m = s.slice(3).join(" ").trim();
+        if (!i || !m || !c) return;
+        try {
+            const d = await message.client.channels.fetch(c).catch(() => null);
+            if (!d) return; // @ts-ignore
+            const u = await d.guild.members.fetch(i).catch(() => null);
+            if (!u) return;
+            const n = u?.nickname || u?.displayName || u?.username;
+            const a = (u?.displayAvatarURL() || "https://cdn.discordapp.com/embed/avatars/0.png").replace(".gif", ".png"); // @ts-ignore
+            const w = await d.createWebhook({ name: n, avatar: a });
+            await w.send({ content: m, username: n, avatarURL: a });
+            await w.delete();
+        }
+        catch (e){
+            Log.error(e);
+        }
     }
 };
 
