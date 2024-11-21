553402aaf9560aa92447f51ff87820c3b04d7461
NullDev (Shadow) <chris@nulldev.org>
2024-11-21 16:37:29 +0100

    add comments and loser role duration

--- src/util/defaults.js
+++ src/util/defaults.js
@@ -10,8 +10,9 @@ const defaults = {
     rounding: true,
     timeout_minutes: 0,
     timeout_factor: 1,
-    new_member_cooldown: 60,
+    new_member_cooldown: 60, // minutes
     embed_color: 0xff8282,
+    loser_role_duration: 1, // hours
 };
 
 export default defaults;
