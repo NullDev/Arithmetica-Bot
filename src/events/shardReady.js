525a0b4d098645b4b2404f4ceac93190a8234d3e
NullDev (Shadow) <chris@nulldev.org>
2025-06-16 12:35:26 +0200

    fix member update evt

--- src/events/shardReady.js
+++ src/events/shardReady.js
@@ -14,18 +14,20 @@ import Log from "../util/log.js";
  */
 const shardReady = async function(client, shard){
     Log.info(`Shard ${shard} is ready!`);
-    const guildCount = await client.guilds.fetch().then(guilds => guilds.size);
+    const guilds = await client.cluster?.fetchClientValues("guilds.cache.size");
+    const guildCount = guilds?.reduce((acc, gc) => Number(acc) + Number(gc), 0);
     await setStatus(client, guildCount);
 
     // Reload guild count every 10 minutes if it changed
     let lastGuildCount = guildCount;
     setInterval(async() => {
-        const newGuildCount = await client.guilds.fetch().then(guilds => guilds.size);
+        const newGuilds = await client.cluster?.fetchClientValues("guilds.cache.size");
+        const newGuildCount = newGuilds?.reduce((acc, gc) => Number(acc) + Number(gc), 0);
 
         if (newGuildCount !== lastGuildCount){
             lastGuildCount = newGuildCount;
             await setStatus(client, newGuildCount);
-            Log.info("Guild count changed to " + newGuildCount + ". Updated activity.");
+            Log.info(`Guild count changed from ${guildCount} to ${newGuildCount} (${(newGuildCount - guildCount) > 0 ? "+" : ""}${(newGuildCount - guildCount)}). Updated activity.`);
         }
     }, 10 * 60 * 1000);
 };
